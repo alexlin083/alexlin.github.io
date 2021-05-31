92bf50769e36d87ffc7e9feb01be83b4afcb7e97
alex <alex3889660@gmail.com>
2021-05-31 21:21:18 +0800

    use promise-race()

--- basic/promise4-race.js
+++ basic/promise4-race.js
@@ -1,6 +1,6 @@
-//promise.all()
+//promise.race()
 let p1 = new Promise((resolve, reject) => {
-  //   setTimeout(reject, 1000, "one");
+  setTimeout(resolve, 1000, "one");
   reject("reject1");
 });
 let p2 = new Promise((resolve, reject) => {
@@ -10,7 +10,7 @@ let p3 = new Promise((resolve, reject) => {
   setTimeout(resolve, 3000, "three");
 });
 let p4 = new Promise((resolve, reject) => {
-  //   setTimeout(reject, 4000, "four");
+  // setTimeout(reject, 4000, "four");
   reject("reject4");
 });
 let p5 = new Promise((resolve, reject) => {
@@ -25,8 +25,6 @@ Promise.race([p1, p2, p3, p4, p5]).then(
     console.log(reason);
   }
 );
-
-//From console:
-//"reject"
-
-//You can also use .catch
+//如果 let p4 內的函式換成reject("reject4");
+//則會因為沒有設定時間而第一個執行
+//若有2個同時間的reject，會執行最上面的 如 p1
