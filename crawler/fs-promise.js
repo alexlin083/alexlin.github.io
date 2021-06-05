387836f20b614532097eb6087c058f8a649136f8
alex <alex3889660@gmail.com>
2021-06-05 11:20:46 +0800

    use fs/promise版本

---
+++ crawler/fs-promise.js
@@ -0,0 +1,61 @@
+//test-promise
+const axios = require("axios");
+const fs = require("fs/promises");
+
+const moment = require("moment");
+const BusinessDay = moment().format("YYYYMMDD");
+// console.log(date);
+
+//promise
+// function readFilePromise() {
+//   return new Promise((resolve, reject) => {
+//     fs.readFile("stock.txt", "utf8", (err, data) => {
+//       if (err) {
+//         reject(err);
+//       }
+//       resolve(data);
+//     });
+//   });
+// }
+
+fs.readFile("stock.txt", "utf8")
+  .then((data) => {
+    return axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
+      params: {
+        response: "json",
+        date: BusinessDay,
+        stockNo: data,
+      },
+    });
+  })
+  .then(function (response) {
+    // console.log(response.data.date);
+    if (response.data.stat === "OK") {
+      console.log(response.data.date);
+      console.log(response.data.title);
+    }
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// readFilePromise()
+//   .then((data) => {
+//     return axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
+//       params: {
+//         response: "json",
+//         date: BusinessDay,
+//         stockNo: data,
+//       },
+//     });
+//   })
+//   .then(function (response) {
+//     // console.log(response.data.date);
+//     if (response.data.stat === "OK") {
+//       console.log(response.data.date);
+//       console.log(response.data.title);
+//     }
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
