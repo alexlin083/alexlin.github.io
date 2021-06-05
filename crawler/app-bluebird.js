6974770e7f081a5c2902a1b8935d1b15e638ba67
alex <alex3889660@gmail.com>
2021-06-05 11:27:32 +0800

    add Promise-bluebird

---
+++ crawler/app-bluebird.js
@@ -0,0 +1,41 @@
+//test-promise
+const axios = require("axios");
+const fs = require("fs");
+const moment = require("moment");
+const BusinessDay = moment().format("YYYYMMDD");
+const Promise = require("bluebird");
+// console.log(date);
+
+//promise
+// function readFilePromise() {
+//   return new Promise((resolve, reject) => {
+//     fs.readFile("stock.txt", "utf8", (err, data) => {
+//       if (err) {
+//         reject(err);
+//       }
+//       resolve(data);
+//     });
+//   });
+// }
+const readFileBlue = Promise.promisify(fs.readFile);
+
+readFileBlue("stock.txt", "utf8")
+  .then((data) => {
+    return axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
+      params: {
+        response: "json",
+        date: BusinessDay,
+        stockNo: data,
+      },
+    });
+  })
+  .then(function (response) {
+    // console.log(response.data.date);
+    if (response.data.stat === "OK") {
+      console.log(response.data.date);
+      console.log(response.data.title);
+    }
+  })
+  .catch((err) => {
+    console.log(err);
+  });
