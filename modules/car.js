d7ed0c5bcca2d08c2461b2965c2543d5ecac6b91
alex <alex3889660@gmail.com>
2021-06-18 11:07:03 +0800

    add test modules one

--- modules/car.js
+++ modules/car.js
@@ -5,10 +5,13 @@ const bic = {
 };
 
 exports.name = "Ben";
+
+//建立一個getColor並回傳該值
 exports.getColor = function () {
   return bic.color;
 };
 
+//建立一個setColor並回傳該值
 exports.setColor = function (color) {
   if (color == "Red" || color == "Blue") {
     bic.color = color;
--- modules/index.js
+++ modules/index.js
@@ -1,7 +1,11 @@
+//引用car
 const item = require("./car");
+
+//測試抓到甚麼資料
 console.log(item);
 console.log(item.getColor());
 
+//更改car裡面的setColor物件內容
 item.setColor("Red");
 
 console.log(item.getColor());
