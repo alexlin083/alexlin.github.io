af0d13252839d31e2d5a40f4b90a93f248b0a5b1
alex <alex3889660@gmail.com>
2021-06-19 18:44:38 +0800

    update module.exports = router;

--- myweb/routes/index.js
+++ myweb/routes/index.js
@@ -4,9 +4,11 @@ var router = express.Router();
 /* GET home page. */
 router.get("/", function (req, res, next) {
   console.log(req.cookies);
+  req.session.username = "tom";
+  req.session.email = "email";
   res.cookie("name", "banana", {
     maxAge: 10000,
-    // httpOnly: true,
+    httpOnly: true, //瀏覽器的console上無法抓到，
   });
   res.render("index", { title: "Express" });
 });
