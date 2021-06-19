29dfda5467397ffbb7a51e0253f8c2c04f746a3b
alex <alex3889660@gmail.com>
2021-06-19 18:35:57 +0800

    module api & stock Router , 404 & 500 page

--- simple-express/server.js
+++ simple-express/server.js
@@ -30,6 +30,11 @@ app.set("views", "views");
 // 告訴 express 我們用的 view engine 是 pug
 app.set("view engine", "pug");
 
+let stockRouter = require("./routes/stock");
+app.use("/stock", stockRouter);
+let apiRouter = require("./routes/api");
+app.use("/api", apiRouter);
+
 //路由router
 app.get("/", function (req, res) {
   // res.send("這是express測試port 3000");
@@ -48,8 +53,16 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
 
-let stockRouter = require("./routes/stock");
-app.use("/stock", stockRouter);
+app.use(function (req, res, next) {
+  res.status(404);
+  res.render("404");
+});
+
+app.use(function (err, req, res, next) {
+  console.log(err.message);
+  res.status(500);
+  res.send("500 - Internal Server XXXXXXXX 請找管理員");
+});
 
 app.listen(port, async () => {
   await connection.connectAsync();
