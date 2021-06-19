2d61ece647bd20d48d27b87ace06b61465a34455
alex <alex3889660@gmail.com>
2021-06-19 18:46:31 +0800

    use jquery.ajax & axios & fetch for index

--- simple-express/public/javascript/api.js
+++ simple-express/public/javascript/api.js
@@ -1,21 +1,57 @@
 //用jquery.ajax取
 
-$(function () {
-  //   $.ajax({
-  //     method: "GET",
-  //     url: '/api/stock',
-  //     dataType: "json",
-  //   }).done(function (data) {
-  //     console.log(data);
-  //   });
+//ajax
+// $.ajax({
+//   method: "GET",
+//   url: "/api/stock",
+//   dataType: "json",
+// }).done(function (data) {
+//   console.log(data);
+// });
 
-  //axios
-  axios
-    .get("/api/stocks")
-    .then((res) => {
-      console.log(res.data);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-});
+$.ajax({
+  method: "GET",
+  url: "/api/stocks",
+  dataType: "json",
+})
+  .done(function (data) {
+    console.log(data);
+  })
+  .fail(function (error) {
+    console.log(error);
+  })
+  .always(function () {
+    console.log("complete");
+  });
+//axios
+axios
+  .get("/api/stocks")
+  .then((res) => {
+    console.log(res.data);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+//fetch
+fetch("/api/stocks")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
+
+// Vue?
+// $(function () {
+//     new Vue({
+//         el: "#app",
+//         data: {
+//             stock: [],
+//         },
+//         beforeMount: async function () {
+//             let response = await fetch("api/stock");
+//             this.stocks = await response.json();
+//         },
+//     })
+// })
